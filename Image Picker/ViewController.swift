3fc1f0cda23e6f2eb9eac725f470afde59a64906
Marwan Alani <marwan@alani.ca>
2017-02-23 18:08:16 -0500

    Added Image & Activity pickers

--- Image Picker/ViewController.swift
+++ Image Picker/ViewController.swift
@@ -9,17 +9,30 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    /// The call back for pressing the button
+    @IBAction func experiment(_ sender: Any) {
+        //imagePicker()
+        activityController()
     }
-
-
+    
+    /// Display the default image picker (Modal View)
+    func imagePicker() {
+        let nextController = UIImagePickerController()
+        self.present(nextController, animated: true, completion: nil)
+    }
+    
+    
+    /// Display the default activity picker (Modal View)
+    func activityController() {
+        let image = UIImage()
+        let controller = UIActivityViewController(activityItems: [image], applicationActivities: nil)
+        self.present(controller, animated: true, completion: nil)
+    }
 }
 
